8e7eb53bcc359a06aabab3b4c5a3028f6ecdd695
Chinmoy <chinmoym2004@gmail.com>
2020-04-28 21:29:34 +0530

    Safari support - test 1

--- site.js
+++ site.js
@@ -33,3 +33,17 @@ navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(func
 window.addEventListener('appinstalled', (evt) => {
   console.log('a2hs installed');
 });
+
+
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}

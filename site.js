12a2573cc171ef1d31bd7f8142f8b5138cd02010
Chinmoy <chinmoym2004@gmail.com>
2020-04-28 18:16:47 +0530

    Test 1

--- site.js
+++ site.js
@@ -2,30 +2,34 @@ navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(func
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+// let deferredPrompt;
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent the mini-infobar from appearing on mobile
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can install the PWA
+//   showInstallPromotion();
+// });
+
+// let el = document.getElementById("installPWA");
+
+// el.addEventListener('click', (e) => {
+// 	event.preventDefault();
+// 	// Hide the app provided install promotion
+//   hideMyInstallPromotion();
+//   // Show the install prompt
+//   deferredPrompt.prompt();
+//   // Wait for the user to respond to the prompt
+//   deferredPrompt.userChoice.then((choiceResult) => {
+//     if (choiceResult.outcome === 'accepted') {
+//       console.log('User accepted the install prompt');
+//     } else {
+//       console.log('User dismissed the install prompt');
+//     }
+//   })
+// })
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
 });
-
-let el = document.getElementById("installPWA");
-
-el.addEventListener('click', (e) => {
-	event.preventDefault();
-	// Hide the app provided install promotion
-  hideMyInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-  })
-})
